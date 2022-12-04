6f41039e6463a7ee6bb24a6946a6ab585d24156e
Abhi-dot-7528 <abhinav7528@gmail.com>
2022-12-05 00:07:47 +0530

    Created snake.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from snake import Snake
 
 # Setup Screen
 screen = Screen()
@@ -8,28 +9,14 @@ screen.bgcolor("black")
 screen.title("Serpente")
 screen.tracer(0)
 
-# Setup Snake
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for coordinates in starting_positions:
-    new_sq = Turtle()
-    new_sq.color("white")
-    new_sq.shape("square")
-    new_sq.penup()
-    new_sq.goto(coordinates)
-    segments.append(new_sq)
+# Create snake
+snake = Snake()
 
 # Animate snake movements
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for num_of_seg in range(len(segments) - 1, 0, -1):
-        new_x = segments[num_of_seg-1].xcor()
-        new_y = segments[num_of_seg-1].ycor()
-        segments[num_of_seg].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
